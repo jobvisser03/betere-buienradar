f47fc92cec3112197a5d6471f56dfbea5cc505c3
jobvisser03 <jobvisserhcm@gmail.com>
2020-04-16 21:42:17 +0200

    net niets ingelezen

--- src/notebooks/exploration.py
+++ src/notebooks/exploration.py
@@ -4,16 +4,31 @@ import pandas as pd
 import netCDF4
 
 from IPython import InteractiveShell 
-InteractiveShell.ast_node_interactivity = 'all
-
+InteractiveShell.ast_node_interactivity = 'all'
+import os 
+os.chdir('.')
 #%%
-
-
-url='http://geoport-dev.whoi.edu/thredds/dodsC/HUDSON_SVALLEY/5951adc-a1h.nc'
-vname = 'Tx_1211'
-station = 0
+# url='ftp://data.knmi.nl/download/Actuele10mindataKNMIstations/1/noversion/2020/04/16/KMDS__OPER_P___10M_OBS_L2_0000.nc'
+url='../../data/KMDS__OPER_P___10M_OBS_L2_0020.nc'
+vname = 'ta' #Air Temperature 1 Min Average
+station = 1
 nc = netCDF4.Dataset(url)
 times = nc.variables['time']
 h = nc.variables[vname]
 jd = netCDF4.num2date(times[:],times.units)
+jd
 
+# %%
+hs = pd.Series(h[:,station],index=jd)
+
+# %%
+nc.dimensions
+# h[:,]
+
+# %%
+times[:]
+times.units
+
+# %% 
+
+# %%
